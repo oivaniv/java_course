daae844eae261e735354437ad47c3fbd221fcf6d
mroleh <oivaniv@n-ix.com>
2019-03-22 00:04:31 +0200

    test commit

--- src/test/java/Conditionals.java
+++ src/test/java/Conditionals.java
@@ -2,6 +2,10 @@ public class Conditionals {
     public static void main(String[] args){
         System.out.println("Hello");
 
+        if (true){
+            System.out.println("Good");
+
+        }
         System.out.println("Goodbye");
     }
 }
